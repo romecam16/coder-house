14b98d5a640057e2f25d1da6a1f31a477587247b
Camilo Romero <camilo.romero@geniussportsmedia.com>
2021-09-11 15:38:15 -0500

    Add Usuario Class challenge

---
+++ Usuario.js
@@ -0,0 +1,46 @@
+class Usuario {
+
+    constructor(nombre, apellido, libros, mascotas) {
+        this.nombre = nombre //String
+        this.apellido = apellido //String
+        this.libros = libros //Object[]
+        this.mascotas = mascotas // String []
+    }
+
+    getFullName(){
+        return `${this.nombre} ${this.apellido}`
+    }
+
+    addMascota(nombreMascota){
+        this.mascotas.push(nombreMascota)
+    }
+
+    countMascotas(){
+        return this.mascotas.length
+    }
+
+    addBook(nombre, autor){
+        this.libros.push({"nombre": nombre, "autor": autor})
+    }
+
+    getBookNames(){
+        const nombreLibro = []
+        for (let i = 0; i < this.libros.length; i++) {
+            nombreLibro.push(` ${this.libros[i].nombre}`)
+        }
+        return nombreLibro
+    }
+}
+
+const usuario =
+    new Usuario("Camilo",
+        "Romero",
+        [{"nombre": "Cien años de soledad", "autor": "Gabriel Garcia Marquez"}, {"nombre": "Rayuela", "autor": "Julio Cortazar"}],
+        ["Lucky", "Luna"])
+
+
+console.log(`El nombre del usuario es ${usuario.getFullName()}`)
+usuario.addMascota("Coco")
+console.log(`El numero de mascotas del usuario es ${usuario.countMascotas()}`)
+usuario.addBook("Pedro Paramo", "Juan Rulfo")
+console.log(`Los nombres de los libros del usuario son${usuario.getBookNames()}`)
